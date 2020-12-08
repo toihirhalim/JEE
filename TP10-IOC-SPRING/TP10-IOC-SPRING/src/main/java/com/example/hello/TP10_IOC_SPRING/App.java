ba79cf678dbba299b6f606e6350a93d52675de3a
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2020-12-08 19:10:53 +0100

    TP10 created

---
+++ TP10-IOC-SPRING/TP10-IOC-SPRING/src/main/java/com/example/hello/TP10_IOC_SPRING/App.java
@@ -0,0 +1,13 @@
+package com.example.hello.TP10_IOC_SPRING;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
