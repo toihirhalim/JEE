d7f684404d84253a59181772bcc1a3bf935557d7
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2020-12-10 11:56:36 +0100

    TP13 created

---
+++ TP13-SpringBoot-Entities/src/main/java/com/example/demo/Tp13SpringBootEntitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp13SpringBootEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp13SpringBootEntitiesApplication.class, args);
+	}
+
+}
