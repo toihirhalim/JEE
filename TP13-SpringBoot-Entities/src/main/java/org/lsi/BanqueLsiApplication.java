f782a067407552534045c573c50b0a455a0352d9
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2020-12-12 21:00:57 +0100

    update

--- TP13-SpringBoot-Entities/src/main/java/org/lsi/BanqueLsiApplication.java
+++ TP13-SpringBoot-Entities/src/main/java/org/lsi/BanqueLsiApplication.java
@@ -1,5 +1,10 @@
 package org.lsi;
 
-public class BanqueLsiApplication {
+import org.springframework.boot.SpringApplication;
 
+
+public class BanqueLsiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueLsiApplication.class, args);
+	}
 }
