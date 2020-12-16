4c63cada4b202012d79324850ecf03a0db16e48c
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2020-12-16 16:21:24 +0100

    tp 14 created

---
+++ TP14-SpringBoot-Metier-Web/src/main/java/com/example/demo/Tp14SpringBootMetierWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp14SpringBootMetierWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp14SpringBootMetierWebApplication.class, args);
+	}
+
+}
