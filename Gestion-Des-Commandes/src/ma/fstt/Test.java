8cc96ae25a7377be8c3d11ca9e40686cad3ff619
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2020-12-29 17:25:22 +0100

    database working fine

--- Gestion-Des-Commandes/src/ma/fstt/Test.java
+++ Gestion-Des-Commandes/src/ma/fstt/Test.java
@@ -4,14 +4,48 @@ import java.sql.SQLException;
 
 import ma.fstt.dao.ClientDAO;
 import ma.fstt.dao.CommandeDAO;
+import ma.fstt.dao.LigneCommandeDAO;
 import ma.fstt.dao.ProduitDAO;
 import ma.fstt.entities.Client;
 import ma.fstt.entities.Commande;
+import ma.fstt.entities.LigneCommande;
 import ma.fstt.entities.Produit;
 
 public class Test {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		ClientDAO clientDAO = new ClientDAO();
+		ProduitDAO produitDAO = new ProduitDAO();
+		CommandeDAO commandeDAO = new CommandeDAO();
+		LigneCommandeDAO ligneCommandeDAO = new LigneCommandeDAO();
+		
+		
+		//ligne de commande test
+		
+		/*	
+	  	LigneCommande ligneCommande1 = new LigneCommande(0, 2, 2, 5);
+		LigneCommande ligneCommande2 = new LigneCommande(0, 1, 2, 1);
+		LigneCommande ligneCommande3 = new LigneCommande(0, 2, 1, 3);
+		ligneCommandeDAO.ajouterLigneCommande(ligneCommande1);
+		ligneCommandeDAO.ajouterLigneCommande(ligneCommande2);
+		ligneCommandeDAO.ajouterLigneCommande(ligneCommande3);
+		
+		LigneCommande ligneCommande = ligneCommandeDAO.trouverById(7);
+		
+		System.out.println(ligneCommande);
+		
+		ligneCommande.setQtte(10);
+		
+		ligneCommandeDAO.updateLigneCommande(ligneCommande);
+		
+		ligneCommandeDAO.deleteLigneCommande(ligneCommande);
+		
+		for (LigneCommande lctmp : ligneCommandeDAO.listLigneCommande()) {
+			
+			System.out.println(lctmp.toString());
+			
+		}*/
+		
 		
 		//commande test
 				/*CommandeDAO commandeDAO = new CommandeDAO();
@@ -41,7 +75,7 @@ public class Test {
 		
 		
 		//produit test
-		/*ProduitDAO produitDAO = new ProduitDAO();
+		/*
 
 		Produit produit = new Produit(0, "produit 3", 20);
 		produitDAO.ajouterProduit(produit);
@@ -66,7 +100,7 @@ public class Test {
 		
 		
 		//client test
-		/*ClientDAO clientDAO = new ClientDAO();
+		/*
 
 		Client client = new Client(0, "client 3");
 		clientDAO.ajouterClient(client);
--- Gestion-Des-Commandes/src/ma/fstt/dao/LigneCommandeDAO.java
+++ Gestion-Des-Commandes/src/ma/fstt/dao/LigneCommandeDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import ma.fstt.entities.LigneCommande;
@@ -17,6 +18,7 @@ public class LigneCommandeDAO implements LigneCommandeRepository {
 	protected Statement statement ;
 	protected PreparedStatement preparedStatement ;
 	protected  ResultSet resultSet ;
+	
 	public LigneCommandeDAO() throws ClassNotFoundException, SQLException {
 		super();
 		// TODO Auto-generated constructor stub
@@ -25,27 +27,81 @@ public class LigneCommandeDAO implements LigneCommandeRepository {
 	@Override
 	public LigneCommande trouverById(int id) throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select *  from ligne_commande where idLigne = ?";
+		
+		LigneCommande ligneCommande = null;
+		
+		this.preparedStatement = this.connection.prepareStatement(sql);
+		
+		this.preparedStatement.setInt(1,id);
+		
+		this.resultSet = this.preparedStatement.executeQuery();
+		
+		while(this.resultSet.next()) {
+		
+			ligneCommande = new LigneCommande(this.resultSet.getInt(1), this.resultSet.getInt(2), this.resultSet.getInt(3), this.resultSet.getInt(4));
+			
+			break ;
+			
+		}
+		
+		return ligneCommande;
 	}
 	@Override
 	public void ajouterLigneCommande(LigneCommande ligneCommande) throws SQLException {
 		// TODO Auto-generated method stub
+		String sql = "insert into ligne_commande (idCommande, idProduit, qtte) values (?, ?, ?)";
 		
+		this.preparedStatement = this.connection.prepareStatement(sql);
+
+		this.preparedStatement.setInt(1, ligneCommande.getIdCommande());
+		this.preparedStatement.setInt(2, ligneCommande.getIdProduit());
+		this.preparedStatement.setInt(3, ligneCommande.getQtte());
+		
+		this.preparedStatement.execute();
 	}
 	@Override
 	public List<LigneCommande> listLigneCommande() throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		List<LigneCommande> list = new ArrayList() ;
+		this.statement = this.connection.createStatement();
+		
+		String sql = "Select * from ligne_commande";
+		
+		this.resultSet = this.statement.executeQuery(sql);
+		
+		while(this.resultSet.next()) {
+			
+			list.add(new LigneCommande(this.resultSet.getInt(1), this.resultSet.getInt(2), this.resultSet.getInt(3), this.resultSet.getInt(4)));
+			
+		}
+		
+		return list;
 	}
 	@Override
 	public void updateLigneCommande(LigneCommande ligneCommande) throws SQLException {
 		// TODO Auto-generated method stub
+		String sql = "update  ligne_commande set  idCommande = ?, idProduit = ?, qtte =? where idLigne = ?";
 		
+		this.preparedStatement = this.connection.prepareStatement(sql);
+		
+		this.preparedStatement.setInt(1, ligneCommande.getIdCommande());
+		this.preparedStatement.setInt(2, ligneCommande.getIdProduit());
+		this.preparedStatement.setInt(3, ligneCommande.getQtte());
+		this.preparedStatement.setInt(4, ligneCommande.getId());
+		
+		this.preparedStatement.execute();
 	}
 	@Override
 	public void deleteLigneCommande(LigneCommande ligneCommande) throws SQLException {
 		// TODO Auto-generated method stub
+		String sql = "delete  from ligne_commande  where idLigne = ?";
 		
+		this.preparedStatement = this.connection.prepareStatement(sql);
+		
+		this.preparedStatement.setInt(1, ligneCommande.getId());
+		
+		this.preparedStatement.execute();
 	}
 	
 	
